e2c3fc40815347d3a9511a7d7ed0eb8bb27ae4f6
Ritwick Dey <ritwickdey@outlook.com>
2018-03-22 22:34:38 +0530

    refactored

--- face-detection.js
+++ face-detection.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+const detectFace = () => {
   const img = document.querySelector('#targetImg');
   const faceDetector = new FaceDetector();
   const scale = img.width / img.naturalWidth;
@@ -6,18 +6,36 @@ window.onload = () => {
     .detect(img)
     .then(faces => faces.map(face => face.boundingBox))
     .then(faceBoxes => {
-      faceBoxes.forEach(({ height, width, top, left, right }) => {
-        const div = document.createElement('div');
-        div.className = 'face-box';
-        div.style.cssText = `
-            top: ${top * scale}px;
-            left: ${left * scale}px;
-            right: ${right * scale}px;
-            height: ${height * scale}px;
-            width: ${width * scale}px;
-          `;
+      faceBoxes.forEach(faceBox => {
+        const { height, width, top, left } = faceBox;
+        const div = drawFaceBox(height, width, top, left, scale);
         img.parentElement.appendChild(div);
       });
     })
-    .catch(() => console.log('Something Went Wrong'));
+    .catch(w => console.log('Something Went Wrong', w));
+};
+
+const drawFaceBox = (height, width, top, left, scale) => {
+  const div = document.createElement('div');
+  div.className = 'face-box';
+  div.style.cssText = `
+    top: ${top * scale}px;
+    left: ${left * scale}px;
+    height: ${height * scale}px;
+    width: ${width * scale}px;
+  `;
+  return div;
+};
+
+const clearFaceBox = () => {
+  [...document.getElementsByClassName('face-box')].forEach(e => e.remove());
+};
+
+window.onload = () => {
+  clearFaceBox();
+  detectFace();
+};
+window.onresize = () => {
+  clearFaceBox();
+  detectFace();
 };
