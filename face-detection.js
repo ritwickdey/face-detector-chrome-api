6d29f4242836ba764872a3d766451d65a9c333a4
Ritwick Dey <ritwickdey@outlook.com>
2018-04-14 13:57:53 +0530

    immediate function expresson

--- face-detection.js
+++ face-detection.js
@@ -1,47 +1,40 @@
-const detectFace = () => {
-  if(!window.FaceDetector) return console.log('Unsupported Version or Feature is not enabled')
-  const img = document.querySelector('#targetImg');
-  const faceDetector = new FaceDetector();
-  const scale = img.width / img.naturalWidth;
-  clearFaceBox();
-  faceDetector
-    .detect(img)
-    .then(faces => 
-      faces.map(face => face.boundingBox)
-    )
-    .then(faceBoxes => {
-      faceBoxes.forEach(faceBox => {
-        const { 
-          height, width, top, left 
-        } = faceBox;
-        const div = drawFaceBox(
-          height, width, top, left, scale
-        );
-        img.parentElement.appendChild(div);
-      });
-    })
-    .catch(err => console.log(err));
-};
+(function() {
+  if (!window.FaceDetector)
+    return console.log('Unsupported Version or Feature is not enabled');
 
-const drawFaceBox = (height, width, top, left, scale) => {
-  const div = document.createElement('div');
-  div.className = 'face-box';
-  div.style.cssText = `
-    top: ${top * scale}px;
-    left: ${left * scale}px;
-    height: ${height * scale}px;
-    width: ${width * scale}px;
-  `;
-  return div;
-};
+  const detectFace = () => {
+    const img = document.querySelector('#targetImg');
+    const faceDetector = new FaceDetector();
+    const scale = img.width / img.naturalWidth;
+    clearFaceBox();
+    faceDetector
+      .detect(img)
+      .then(faces => faces.map(face => face.boundingBox))
+      .then(faceBoxes => {
+        faceBoxes.forEach(faceBox => {
+          const { height, width, top, left } = faceBox;
+          const div = drawFaceBox(height, width, top, left, scale);
+          img.parentElement.appendChild(div);
+        });
+      })
+      .catch(err => console.log(err));
+  };
 
-const clearFaceBox = () => {
-  [...document.getElementsByClassName('face-box')].forEach(e => e.remove());
-};
+  const drawFaceBox = (height, width, top, left, scale) => {
+    const div = document.createElement('div');
+    div.className = 'face-box';
+    div.style.cssText = `
+      top: ${top * scale}px;
+      left: ${left * scale}px;
+      height: ${height * scale}px;
+      width: ${width * scale}px;
+    `;
+    return div;
+  };
 
-window.onload = () => {
-  detectFace();
-};
-window.onresize = () => {
-  detectFace();
-};
+  const clearFaceBox = () => {
+    [...document.getElementsByClassName('face-box')].forEach(e => e.remove());
+  };
+
+  window.detectFace =  detectFace;
+})();
